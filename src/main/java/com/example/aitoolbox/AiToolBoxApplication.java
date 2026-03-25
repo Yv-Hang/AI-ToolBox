e8b3a9dab6d57ad3704d137aa6bf353b0f266123
ZY-Hang <z2069966157@163.com>
2026-03-25 10:25:32 +0900

    [init]项目初始化

---
+++ src/main/java/com/example/aitoolbox/AiToolBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiToolBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aitoolbox/AiToolBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitoolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
